0735b2ba536061e5eb8967e4230ee7cc805e42d3
zRenegado <gui3033@gmail.com>
2018-05-31 15:34:40 -0300

    1.0.0.4 estavel

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 ## i3-desktop
   Configuração do i3, polybar, rofi Para usar é nessario previamente instalar: i3-gaps, polybar, dunst, xdotool, ttf-fira-sans, adobe-source-code-pro-fonts, jsoncpp, rofi e community/ttf-font-awesome.
+
+[Este projeto é um fork parcial de Sistematico](https://github.com/sistematico/majestic)
+
 ## Súmario
+
 - [i3-gaps](https://github.com/zRenegado/i3-desktop/blob/master/.config/i3/config)
 - [polybar](https://github.com/zRenegado/i3-desktop/blob/master/.config/polybar/config)
-- [i3blocks](https://github.com/zRenegado/i3-desktop/tree/master/.config/i3blocks)
+- [i3blocks](https://github.com)
 - [rofi](https://github.com/zRenegado/i3-desktop/blob/master/.config/rofi/config.rasi)
 - [~/.local/bin](https://github.com/zRenegado/i3-desktop/tree/master/.local/bin)
 - [~/.local/share](https://github.com/zRenegado/i3-desktop/tree/master/.local/share)
@@ -13,28 +17,25 @@
 
 Inicialmente instale os pacotes:
 
-i3-gaps polybar dunst xdotool ttf-fira-sans adobe-source-code-pro-fonts 
-jsoncpp rofi ttf-font-awesome git
+i3-gaps polybar dunst xdotool ttf-fira-sans adobe-source-code-pro-fonts jsoncpp rofi ttf-font-awesome git
 
 Usando o git:
 
-clone o repositorio e mova manuamente os arquivos, se houver alguma 
-configuração já existente faça backup
+clone o repositorio e mova manuamente os arquivos, se houver alguma configuração já existente faça backup
 
 	$cd /tmp && git clone https://github.com/zRenegado/i3-desktop.git
 	copie os arquivos, de configuração:
 	$cp -R i3-desktop/.config  ~/.config
 	$cp -R i3-desktop/.local ~/.local
-	$cd
 
-Se necessario forneça a permissão de execussão,"$ chmod +x" aos scripts em 
-~/.config/polybar/scripts e ~/.local/bin, exemplo:
+Usando o instalador automatico:
 
+	bash <(curl -s -o 'https://github.com/zRenegado/i3-desktop/raw/master/Script%20de%20Instala%C3%A7%C3%A3o/script_de_instalacao_config_i3.sh')>
+
+Se necessario forneça a permissão de execussão,"$ chmod +x" aos scripts em ~/.config/polybar/scripts e ~/.local/bin, o primeiro exemplo é dando permissão a qualquer arquivo .sh e o segundo a um arquivo especifico:
 	$chmod +x ~/.local/bin/*.sh 
-
-*Obs.: Testa no ArchLinux em 31/05/2018
+	$chmod +x ~/.local/bin/screenshot.sh
 ## ScreenShots
 
 ![Screenshot #1][screenshot1]
-
-[screenshot1]:https://raw.githubusercontent.com/zRenegado/i3-desktop/master/.Screeshot/i3.png "Screnshot #1"
+[screenshot1]:https://github.com/zRenegado/i3-desktop/blob/master/.Screeshot/i3.png "Screnshot #1"
--- Script de Instalação/script_de_instalacao_config_i3.sh
+++ Script de Instalação/script_de_instalacao_config_i3.sh
@@ -24,15 +24,15 @@ done
 [ ! -d ${HOME}/.config/i3 ] && mkdir -p ${HOME}/.config/i3
 
 echo "Instalando as configs do i3..."
-curl -s -o ${HOME}/.config/i3/config 'https://raw.githubusercontent.com/sistematico/majestic/master/.config/i3/config'
+curl -s -o ${HOME}/.config/i3/config 'https://raw.githubusercontent.com/zRenegado/i3-desktop/master/.config/i3/config'
 
 # PolyBar
 [ ! -d ${HOME}/.config/polybar ] && mkdir -p ${HOME}/.config/polybar
 
 echo "Instalando as configs da polybar..."
-curl -s -o ${HOME}/.config/polybar/launch.sh 'https://raw.githubusercontent.com/sistematico/majestic/master/.config/polybar/launch.sh'
+curl -s -o ${HOME}/.config/polybar/launch.sh 'https://raw.githubusercontent.com/zRenegado/i3-desktop/master/.config/polybar/launch.sh'
 chmod +x ${HOME}/.config/polybar/launch.sh
-curl -s -o ${HOME}/.config/polybar/config "https://raw.githubusercontent.com/sistematico/majestic/master/.config/polybar/config"
+curl -s -o ${HOME}/.config/polybar/config "https://github.com/zRenegado/i3-desktop/raw/master/.config/polybar/config"
 
 # Polybar Scripts
 [ ! -d ${HOME}/.config/polybar/scripts ] &&	mkdir -p ${HOME}/.config/polybar/scripts
@@ -41,13 +41,13 @@ scripts=("anews.py" "btc.sh" "crypto.py" "janela.sh" "logs.sh" "pkg.sh" "strcut.
 gmail=("auth.py"  "client_secrets.json"  "credentials.json"  "launch.py"  "preview.png"  "readme.md")
 
 for script in ${scripts[@]}; do
-	curl -s -o ${HOME}/.config/polybar/scripts/${script} "https://raw.githubusercontent.com/sistematico/majestic/master/.config/polybar/scripts/${script}"
+	curl -s -o ${HOME}/.config/polybar/scripts/${script} "https://raw.githubusercontent.com/zrenegado/i3-desktop/master/.config/polybar/scripts/${script}"
 done
 
 [ ! -d ${HOME}/.config/polybar/scripts/gmail ] && mkdir -p ${HOME}/.config/polybar/scripts/gmail
 
 for gm in ${gmail[@]}; do
-	curl -s -o ${HOME}/.config/polybar/scripts/gmail/${gm} "https://raw.githubusercontent.com/sistematico/majestic/master/.config/polybar/scripts/gmail/${gm}"
+	curl -s -o ${HOME}/.config/polybar/scripts/gmail/${gm} "https://raw.githubusercontent.com/zrenegado/i3-desktop/master/.config/polybar/scripts/gmail/${gm}"
 done
 
 # Scripts auxiliares
@@ -55,12 +55,12 @@ done
 
 localbinscripts=("beats.sh" "lockscreen" "lockscreen.firefox" "lockscreen.quotes" "lockscreen.simple" "notification.sh" "otf2ttf.ff" "screencast" "screenshot" "steam-launcher" "xautolock")
 for localbinscript in ${localbinscripts[@]}; do
-	curl -s -o ${HOME}/.local/bin/${localbinscript} "https://raw.githubusercontent.com/sistematico/majestic/master/.local/bin/${localbinscript}"
+	curl -s -o ${HOME}/.local/bin/${localbinscript} "https://raw.githubusercontent.com/zrenegado/i3-desktop/master/.local/bin/${localbinscript}"
 done
 
 # dunst
 [ ! -d ${HOME}/.config/dunst ] && mkdir -p ${HOME}/.config/dunst
-curl -s -o ${HOME}/.config/dunst/dunstrc 'https://raw.githubusercontent.com/sistematico/majestic/master/.config/dunst/dunstrc'
+curl -s -o ${HOME}/.config/dunst/dunstrc 'https://raw.githubusercontent.com/zrenegado/i3-desktop/master/.config/dunst/dunstrc'
 
 # rofi
 if [ -d ${HOME}/.config/rofi ]; then
@@ -79,14 +79,14 @@ fi
 
 [ ! -d ${HOME}/.local/share/rofi/themes/ ] && mkdir -p ${HOME}/.local/share/rofi/themes/
 
-curl -s -o ${HOME}/.config/rofi/config.rasi 'https://raw.githubusercontent.com/sistematico/majestic/master/.config/rofi/config.rasi'
-curl -s -o ${HOME}/.local/share/rofi/themes/lateral.rasi 'https://raw.githubusercontent.com/sistematico/majestic/master/.local/share/rofi/themes/lateral.rasi'
-curl -s -o ${HOME}/.local/share/rofi/themes/dark.rasi 'https://raw.githubusercontent.com/sistematico/majestic/master/.local/share/rofi/themes/dark.rasi'
-curl -s -o ${HOME}/.local/share/rofi/themes/default.rasi 'https://raw.githubusercontent.com/sistematico/majestic/master/.local/share/rofi/themes/default.rasi'
+curl -s -o ${HOME}/.config/rofi/config.rasi 'https://raw.githubusercontent.com/zrenegado/i3-desktop/master/.config/rofi/config.rasi'
+curl -s -o ${HOME}/.local/share/rofi/themes/lateral.rasi 'https://raw.githubusercontent.com/zrenegado/i3-desktop/master/.local/share/rofi/themes/lateral.rasi'
+curl -s -o ${HOME}/.local/share/rofi/themes/dark.rasi 'https://raw.githubusercontent.com/zrenegado/i3-desktop/master/.local/share/rofi/themes/dark.rasi'
+curl -s -o ${HOME}/.local/share/rofi/themes/default.rasi 'https://raw.githubusercontent.com/sistematico/zrenegado/i3-desktop/.local/share/rofi/themes/default.rasi'
 
 rscripts=("alarme" "apps" "configs" "configs.bspwm" "drun" "fap" "git" "janelas" "power" "power.bspwm" "run" "screenshot")
 for rscript in ${rscripts[@]}; do
-	curl -s -o ${HOME}/.config/rofi/scripts/${rscript} "https://raw.githubusercontent.com/sistematico/majestic/master/.config/rofi/scripts/${rscript}"
+	curl -s -o ${HOME}/.config/rofi/scripts/${rscript} "https://raw.githubusercontent.com/zrenegado/i3-desktop/master/.config/rofi/scripts/${rscript}"
 done
 
 # fontawesome
@@ -96,13 +96,13 @@ if [ $? -ne 0 ]; then
 	if [ ! -d ${HOME}/.local/share/fonts ]; then
 		mkdir ${HOME}/.local/share/fonts
 	fi
-	curl -s "https://raw.githubusercontent.com/sistematico/majestic/master/.local/share/fonts/feather.ttf" > ${HOME}/.local/share/fonts/feather.ttf
+	curl -s "https://raw.githubusercontent.com/zrenegado/i3-desktop/master/.local/share/fonts/feather.ttf" > ${HOME}/.local/share/fonts/feather.ttf
 	fc-cache -v -f
 fi
 
 # Wallpaper
-DIR="${HOME}/.local/share/backgrounds"
-WALL="${DIR}/carmine-de-fazio-31543.jpg"
+DIR="${HOME}/.local/share/wallpaper"
+WALL="${DIR}/maxresdefault.jpg"
 
 if [ ! -f $WALL ]; then
 	if [ ! -d $DIR ]; then
@@ -110,7 +110,7 @@ if [ ! -f $WALL ]; then
 	fi
 fi
 
-curl -s "https://i.imgur.com/crTC1QR.jpg" > $WALL
+curl -s "https://i.imgur.com/OPwODDy.jpg" > $WALL
 
 if [ "$DESKTOP_SESSION" == "i3" ]; then
 	i3-msg restart
